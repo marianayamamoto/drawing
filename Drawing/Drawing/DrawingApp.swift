5ffca5a8039f7d2854f2943e75e31e984ff2b463
Mariana Yamamoto <myamamot@lifescan.com>
2021-10-11 08:38:38 -0400

    first project

---
+++ Drawing/Drawing/DrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingApp.swift
+//  Drawing
+//
+//  Created by Mariana Yamamoto on 10/11/21.
+//
+
+import SwiftUI
+
+@main
+struct DrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
