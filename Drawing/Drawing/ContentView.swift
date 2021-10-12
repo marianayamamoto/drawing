511155b6360702240c1083160f100071e2650afa
Mariana Yamamoto <myamamot@lifescan.com>
2021-10-12 09:51:25 -0400

    use image paint to use image in other shapes

--- Drawing/Drawing/ContentView.swift
+++ Drawing/Drawing/ContentView.swift
@@ -7,43 +7,19 @@
 
 import SwiftUI
 
-struct Flower: Shape {
-    var petalOffset: Double = -20
-    var petalWidth: Double = 100
-
-    func path(in rect: CGRect) -> Path {
-        var path = Path()
-        for number in stride(from: 0, to: CGFloat.pi * 2, by: CGFloat.pi / 8) {
-            let rotation = CGAffineTransform(rotationAngle: number)
-            let position =  rotation.concatenating(CGAffineTransform(translationX: rect.width / 2, y: rect.height / 2))
-
-            let originalPetal = Path(ellipseIn: CGRect(x: CGFloat(petalOffset), y: 0, width: CGFloat(petalWidth), height: rect.width / 2))
-            let rotatedPetal = originalPetal.applying(position)
-
-            path.addPath(rotatedPetal)
-        }
-
-        return path
-    }
-}
-
 struct ContentView: View {
     @State private var petalOffset: Double = -20
     @State private var petalWidth: Double = 100
 
     var body: some View {
-        VStack {
-            Flower(petalOffset: petalOffset, petalWidth: petalWidth)
-                .fill(Color.red, style: FillStyle(eoFill: true))
-
-            Text("Offset")
-            Slider(value: $petalOffset, in: -40...40)
-                .padding([.horizontal, .bottom])
-
-            Text("Width")
-            Slider(value: $petalWidth, in: 0...100)
-                .padding(.horizontal)
-        }
+        Text("Hello World")
+            .frame(width: 300, height: 300)
+//            .background(Image("Example"))
+//            .border(ImagePaint(image: Image("Example"), scale: 0.2), width: 30)
+//            .border(ImagePaint(image: Image("Example"), sourceRect: CGRect(x: 0, y: 0.25, width: 1, height: 0.5), scale: 0.1), width: 30)
+        Capsule()
+            .strokeBorder(ImagePaint(image: Image("Example"), scale: 0.1), lineWidth: 20)
+            .frame(width: 300, height: 200)
     }
 }
 
